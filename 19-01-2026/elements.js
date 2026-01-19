64ed6679da33fa52be7afc3dc2447e4295a08a9b
Adan-Ignis <ignaciogomevazquez2000@gmail.com>
2026-01-19 19:39:09 +0100

    Prueba

--- 19-01-2026/elements.js
+++ 19-01-2026/elements.js
@@ -13,16 +13,31 @@ let manejador;
 
 function mostrarFecha() {
     let fecha = new Date(); //Para obtener la fecha
+    //Fecha
+    let anio = fecha.getFullYear();
+    let mes = fecha.getMonth() + 1;
+    let dia = fecha.getDate();
     let hora = fecha.getHours();
     let minuto = fecha.getMinutes();
     let segundo = fecha.getSeconds();
+    // Dia de la semana
+    let diaSemana = fecha.getDay();
 
+    //Día de la semana
+    //let spanDiaSemana = document.getElementById("diaSemana");
+    //Fecha
+    let spanMes = document.getElementById("mes");
+    let spanDia = document.getElementById("dia");
     let spanHora = document.getElementById("horas");
     let spanMinuto = document.getElementById("minutos");
     let spanSegundo = document.getElementById("segundos");
 
-    spanHora.innerHtml = hora.toString().padStart(2, "0");
-    spanMinuto.innerHtml = minuto.toString().padStart(2, "0");
+    //Día de la semana
+    //spanDiaSemana.innerHTML ='';
+    spanMes.innerHTML = mes.toString().padStart(2, "0");
+    spanDia.innerHTML = dia.toString().padStart(2, "0");
+    spanHora.innerHTML = hora.toString().padStart(2, "0");
+    spanMinuto.innerHTML = minuto.toString().padStart(2, "0");
     spanSegundo.innerHTML = segundo.toString().padStart(2, "0");
     //Tambien puede utilizarse: String(segundo).padStart(2, "0");
 }
@@ -30,4 +45,13 @@ function mostrarFecha() {
 function iniciar() {
     mostrarFecha();
     manejador = setInterval(mostrarFecha, 1000);
+}
+
+function parar() {
+    clearInterval(manejador);
+}
+
+function diaSemana() {
+    let semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+    return
 }
