58495572b820f1292a753aa2e3342ea314ab08ab
Adan-Ignis <ignaciogomevazquez2000@gmail.com>
2026-01-20 17:07:31 +0100

    Prueba

---
+++ 20-01-2026/elements.js
@@ -0,0 +1,12 @@
+let numero = Math.floor(Math.random() * 11);
+let nPropuesto
+let intento = 10;
+
+do {
+    if (numero == nPropuesto) {
+        window.print("Lo has adivinado");
+    } else {
+        intento--;
+        windows.print("te quedan " + intento + "intentos");
+    }
+} while (numero != a || intento >= 0);
