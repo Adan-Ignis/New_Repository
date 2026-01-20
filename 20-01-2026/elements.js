5ccbf8deca338aa6b6eab04b0ea4a4587afbf969
Adan-Ignis <ignaciogomevazquez2000@gmail.com>
2026-01-20 18:17:35 +0100

    Prueba

--- 20-01-2026/elements.js
+++ 20-01-2026/elements.js
@@ -12,6 +12,26 @@ do {
 } while (numero != a || intento >= 0);
 
 // Solucion del profesor
-function adivinar() {}
+function adivinar(e) {
+    e.preventDefaul();
+
+    let numero = document.getElementById("numero");
+    let quedan = document.getElementById("quedan");
+    let resultado = document.getElementById("resultado");
+
+    NUM = parseInt(numero.value);
+    console.log(num);
+
+    if(intentos < max_intentos) {
+        if(num == objetivo) {
+            resultado.innerHTML = "ENHORABUENA has aceptado";
+        } else if (num < objetivo) {
+            resultado.innerHTML = "El numero es menor que el objetivo";
+        } else {
+            resultado.innerHTML = "El numero es mayor que el objetivo";
+            // Tambien se puede poner "El numero ${numero} es mayor que el objetivo" para poner valores de una variable sin tener que usar "+". Se conoce como interpolación.
+        }
+    }
+}
 
 function reiniciar() {}
