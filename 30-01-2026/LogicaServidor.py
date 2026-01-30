da1f1d862f338240bd1453a0eaf480bfa38298be
Adan-Ignis <ignaciogomevazquez2000@gmail.com>
2026-01-30 16:47:22 +0100

    Prueba

---
+++ 30-01-2026/LogicaServidor.py
@@ -0,0 +1,54 @@
+# Ignacio Gómez Vázquez
+
+    # "1. Escriba un programa que solicite al usuario que ingrese una cadena. El programa debería mostrar lo siguiente:
+    # (a) El número total de caracteres de la cadena
+    # (b) La cadena repetida 10 veces
+    # (c) El primer carácter de la cadena (recuerde que los índices de cadena empiezan en 0)
+    # (d) Los tres primeros caracteres de la cadena
+    # (e) Los tres últimos caracteres de la cadena
+    # (f) La cadena al revés
+    # (g) El séptimo carácter de la cadena si la cadena es lo suficientemente larga; en caso contrario, un mensaje
+    # (h) La cadena sin el primer y el último carácter
+    # (i) La cadena en mayúsculas
+    # (j) La cadena con cada a reemplazada por una e
+    # (k) La cadena con cada letra reemplazada por un espacio
+
+cadena = input("Dame un numero") # A
+
+for i in range(11): # B
+    print(cadena)
+
+for i in cadena(0): # C
+    print(cadena[i])
+
+for i in cadena(2): # D
+    print(cadena[i])
+
+for i in cadena(-3, -1): # E
+    print(cadena[i])
+
+for i in cadena(0, -1): # F
+    print(cadena[i])
+
+for i in cadena(6, 6): # G
+    if cadena(6) != none:
+        print(cadena[5])
+    else
+        print("La cadena tiene menos de 6 caracteres alfanumericos")
+
+for i in cadena(1,-2, 1): # H
+    print(cadena[i])
+
+for i in cadena(): # I
+    mayuscula = cadena[i]
+    print(mayuscula)
+
+for i in cadena(): # J
+    if cadena[i] == "a":
+        cadena[i] = "e"
+    print(cadena[i])
+
+for i in cadena(): # K
+    if cadena[i] == #caracter alfanumerico
+        cadena[i] = " " #espacio en blanco
+    print cadena[i]
